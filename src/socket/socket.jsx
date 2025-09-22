d76bd2baa7dc3499996cb370b1272d3714f33029
Macala12 <150782120+Macala12@users.noreply.github.com>
2025-09-22 12:34:57 +0100

    update socket.jsx

--- src/socket/socket.jsx
+++ src/socket/socket.jsx
@@ -1,4 +1,7 @@
 import { io } from "socket.io-client";
-const socket = io("https://whot-1.onrender.com");
+
+const socket = io("https://whot-1.onrender.com", {
+  transports: ["websocket"], // helps avoid polling issues on Render
+});
 
 export default socket;
