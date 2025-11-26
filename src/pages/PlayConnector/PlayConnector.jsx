bbc84b660c0121170d3bcda1dd31f0de18b8af74
Macala12 <150782120+Macala12@users.noreply.github.com>
2025-11-27 00:18:48 +0100

    push change back

--- src/pages/PlayConnector/PlayConnector.jsx
+++ src/pages/PlayConnector/PlayConnector.jsx
@@ -5,6 +5,8 @@ import socket from "../../socket/socket";
 import React, { useEffect, useState } from "react";
 
 function PlayConnector() {
+  sessionStorage.removeItem("waitingEndTime");
+  sessionStorage.removeItem("endTime");
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
